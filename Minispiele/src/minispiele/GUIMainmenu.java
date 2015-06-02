8a2173ff8d423ed4619487dfc4e890f8c2bf2536
Windows7 <Windows7@Windows7-PC.fritz.box>
2015-06-02 17:50:30 +0200

    Entfernen der Menüleiste (Menüpunkte können im spielfeld enthalten sein sofern benötigt) sowie hinzufügen von Snakeicons

--- Minispiele/src/minispiele/GUIMainmenu.java
+++ Minispiele/src/minispiele/GUIMainmenu.java
@@ -11,12 +11,12 @@ import minispiele_PingPong.PingPong;
  *
  * @author nmamerow
  */
-public class GUI extends javax.swing.JFrame {
+public class GUIMainmenu extends javax.swing.JFrame {
 
     /**
      * Creates new form GUI
      */
-    public GUI() {
+    public GUIMainmenu() {
         initComponents();
 
     }
@@ -83,21 +83,22 @@ public class GUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMainmenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMainmenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMainmenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIMainmenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new GUI().setVisible(true);
+                new GUIMainmenu().setVisible(true);
             }
         });
     }
