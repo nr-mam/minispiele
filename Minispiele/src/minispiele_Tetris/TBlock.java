6fe0bb6288ee6d11824932223b60816893746b95
Marc <Marc@MarcPC.fritz.box>
2015-06-04 18:00:47 +0200

    Tetris weiter entwickelt.

---
+++ Minispiele/src/minispiele_Tetris/TBlock.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minispiele_Tetris;
+
+/**
+ *
+ * @author Marc
+ */
+public class TBlock {
+
+    public TBlock() {
+    }
+    
+}
